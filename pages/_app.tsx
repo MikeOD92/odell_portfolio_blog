bdcd6ca8ab646c69152a914042e11bbbabbf77e4
Michael O'Dell <michaelodell660@gmail.com>
2023-05-01 15:46:47 -0700

    inital commit, playing with styling home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import react, { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [light, setlight] = useState(false);
+
+  return (
+    <div>
+      <button onClick={() => setlight(!light)}> Change mode</button>
+      <Component {...pageProps} light={light} />;
+    </div>
+  );
 }
