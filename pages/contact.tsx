1155160c636187f2cd56a7d9a1ab6455e7bf428d
Michael O'Dell <michaelodell660@gmail.com>
2024-02-14 12:26:15 -0800

    building contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Gi3DStairs } from "react-icons/gi";
+import Link from "next/link";
+
+export default function contact(props: any) {
+  let light = props.light;
+  return (
+    <div>
+      <div
+        className={`h-full w-full ${
+          light
+            ? "bg-gradient-to-b from-zinc-100 to-zinc-300"
+            : "bg-black text-white"
+        }`}
+      >
+        <Link
+          href="/"
+          className={`absolute right-5 top-2 text-5xl ${
+            light ? "text-black" : "text-white"
+          }`}
+        >
+          <Gi3DStairs />
+        </Link>
+      </div>
+      <div
+        className={`h-screen flex flex-col items-center ${
+          light
+            ? "bg-gradient-to-b from-zinc-100 to-zinc-300 text-black"
+            : "bg-black text-white"
+        }`}
+      >
+        <div
+          className={`mt-24 px-24 p-16 w-2/3 ${
+            light ? "border-black" : "border-white"
+          } border-2`}
+        >
+          <h2 className="titletxt text-6xl">Contact</h2>
+          <ul className="text-4xl tracking-wide leading-loose py-5">
+            <li className="displaytxt">
+              Email :{" "}
+              <span className="titletxt">michaelodell660@gmail.com</span>
+            </li>
+            <li>
+              <Link href="https://github.com/MikeOD92" className="displaytxt">
+                GitHub: <span className="titletxt">github.com/MikeOD92</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="https://www.linkedin.com/in/michaelodell92/"
+                className="displaytxt"
+              >
+                LinkedIn:{" "}
+                <span className="titletxt">
+                  www.linkedin.com/in/michaelodell92
+                </span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -73,7 +73,7 @@ const Home = (props: any) => {
                         : "border-zinc-200 hover:bg-white hover:text-black"
                     } p-5`}
                   >
-                    <p className="text-2xl md:text-3xl displaytxt">Contact</p>
+                    <p className="text-2xl md:text-3xl titletxt">Contact</p>
                   </div>
                 </a>
               </motion.div>
