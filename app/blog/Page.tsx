2f499a0729eb707e6c66bfa854339c8956556f98
Michael O'Dell <michaelodell660@gmail.com>
2024-03-29 12:28:04 -0700

    trying to change casing of exported file name

--- app/blog/Page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import BlogGridDisplay from "../../components/blogComponents/BlogGridDisplay";
-import { getAllPosts } from "../../lib/blogFunctions";
-
-export default async function Page() {
-  const posts = await getAllPosts();
-
-  return <BlogGridDisplay posts={posts} />;
-}
